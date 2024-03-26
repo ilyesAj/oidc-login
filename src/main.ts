590454ec985f52ee31f724bd7cd5f5a6acc43792
ilyesAj <ilyes.ajroud@gmail.com>
2024-03-26 12:35:17 +0100

    intial commit

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,55 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
-
+import axios from 'axios'
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const username = core.getInput('username')
+    const password = core.getInput('password')
+    const url = core.getInput('url')
+    const clientId = core.getInput('client_id')
+    const clientSecret = core.getInput('client_secret')
+    const grantType = core.getInput('grant_type')
+    const scope = core.getInput('scope')
+    const audiance = core.getInput('audiance')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    // Construct the payload based on inputs
+    const payload: any = {
+      client_id: clientId,
+      username: username,
+      password: password,
+    };
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    if (clientSecret) {
+      payload.client_secret = clientSecret;
+    }
+    if (scope) {
+      payload.scope = scope;
+    }
+    if (grantType) {
+      payload.grant_type = grantType;
+    }
+    if (audiance) {
+      payload.audience = audiance;
+    }
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    // Make a request to the OIDC provider
+    const response = await axios.post(url, payload);
+
+    // Extract relevant data from the response
+    const { access_token, refresh_token, expires_in,refresh_expires_in } = response.data;
+
+    // Set the outputs
+    core.setOutput('access_token', access_token);
+    core.setOutput('refresh_token', refresh_token);
+    core.setOutput('expires_in', expires_in);
+    core.setOutput('refresh_expires_in', refresh_expires_in);
+    
+
+
   } catch (error) {
-    // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
